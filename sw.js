08146702f10e693231df5934937d089510c2a644
avi-hub-u <abi.yuu.0018@gmail.com>
2026-05-16 22:42:56 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'talk-with-u-v10';
+const CACHE_NAME = 'talk-with-u-v11';
 const urlsToCache = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', (event) => {
